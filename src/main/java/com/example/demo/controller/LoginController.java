af0cde8349d86893846fa2bcefa2b9669aea345b
qiaopc <908694753@qq.com>
2023-07-21 15:21:20 +0800

    修改路径为login

--- src/main/java/com/example/demo/controller/LoginController.java
+++ src/main/java/com/example/demo/controller/LoginController.java
@@ -8,8 +8,10 @@ import com.example.demo.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/login")
 public class LoginController {
 
     @Autowired
